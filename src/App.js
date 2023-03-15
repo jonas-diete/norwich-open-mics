2c56c15db3c9d925eab31ceda91466edcbd881f0
Jonas Diete <jonas.diete@gmail.com>
2023-03-15 07:51:49 +0000

    table of dates is now separate element

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Overview from './Overview';
+
+function App() {
+  return (
+    <Overview />
+  );
+}
+
+export default App;
---
+++ src/Overview.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function Overview() {
+  return (
+    <table>
+      <tr>
+        <th>Day</th>
+        <th>Place</th>
+        <th>Time</th>
+        <th>Link</th>
+      </tr>
+      <tr>
+        <td>Monday</td>
+        <td>The Rose Tavern</td>
+        <td>8pm-11pm</td>
+        <td>https://www.facebook.com/therosetavern</td>
+      </tr>
+      <tr>
+        <td>Tuesday</td>
+        <td>The Pear Tree Inn</td>
+        <td>8pm-11pm</td>
+        <td>https://www.facebook.com/peartreeinnnorwich</td>
+      </tr>
+      <tr>
+        <td>Tuesday</td>
+        <td>The Brickmakers</td>
+        <td>?</td>
+        <td>https://www.thebrickmakers.com/eventsguide </td>
+      </tr>
+      <tr>
+        <td>Wednesday - every 3rd Wednesday of the Month</td>
+        <td>The Cellar House</td>
+        <td>?</td>
+        <td>https://www.facebook.com/TheCellarHouse</td>
+      </tr>
+      <tr>
+        <td>Thursday</td>
+        <td>The Walnut Tree Shades</td>
+        <td>8pm-11pm</td>
+        <td>https://www.facebook.com/walnut.treeshades.7</td>
+      </tr>
+      <tr>
+        <td>Thursday</td>
+        <td>The Reindeer</td>
+        <td>7pm-11pm</td>
+        <td>https://www.facebook.com/TheReindeerNorwich</td>
+      </tr>
+    </table>
+  );
+}
+
+export default Overview;
