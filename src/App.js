7e549a26139f9297eba6a2fe745ce5b946b87170
Jonas Diete <jonas.diete@gmail.com>
2023-03-16 11:29:35 +0000

    added basic stylesheets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Overview from './Overview';
 import Header from './Header';
 import AddEntry from './AddEntry';
+import './App.css';
 
 const App = () => {
   return (
